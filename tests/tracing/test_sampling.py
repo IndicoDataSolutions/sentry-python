22dc506e6c233d005f096c53323576449c112282
Katie Byers <lobsterkatie@gmail.com>
2020-10-15 21:51:24 -0700

    make unsampled transactions findable on the scope

--- tests/tracing/test_sampling.py
+++ tests/tracing/test_sampling.py
@@ -1,4 +1,6 @@
-from sentry_sdk import start_span, start_transaction
+import pytest
+
+from sentry_sdk import Hub, start_span, start_transaction
 
 
 def test_sampling_decided_only_for_transactions(sentry_init, capture_events):
@@ -32,3 +34,17 @@ def test_no_double_sampling(sentry_init, capture_events):
         pass
 
     assert len(events) == 1
+
+
+@pytest.mark.parametrize("sampling_decision", [True, False])
+def test_get_transaction_and_span_from_scope_regardless_of_sampling_decision(
+    sentry_init, sampling_decision
+):
+    sentry_init(traces_sample_rate=1.0)
+
+    with start_transaction(name="/", sampled=sampling_decision):
+        with start_span(op="child-span"):
+            with start_span(op="child-child-span"):
+                scope = Hub.current.scope
+                assert scope.span.op == "child-child-span"
+                assert scope.transaction.name == "/"
